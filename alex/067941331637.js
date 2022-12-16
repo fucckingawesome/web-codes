5f58559b2606b6605a113974313d7eb1a4fbc91b
fucckingawesome <godknowspenspinning@gmail.com>
2022-12-17 02:21:23 +0800

    console alex

--- alex/067941331637.js
+++ alex/067941331637.js
@@ -1,1 +1,7 @@
-
+export const IASD8O2H3O = {
+    apiKey: "AIzaSyClkGm_12G7cTfKbbKe3LKBaNTOdihnD78",
+    authDomain: "snsdapp.firebaseapp.com",
+    projectId: "snsdapp",
+    storageBucket: "snsdapp.appspot.com",
+    messagingSenderId: "1061275208460",
+}
